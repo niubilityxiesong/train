b377750c206a56fca890693505a1651219672bd8
xie song <563527588@qq.com>
2021-07-25 17:39:30 +0800

    init repo

---
+++ src/main/java/com/example/demo/controller/HelloController.java
@@ -0,0 +1,17 @@
+package com.example.demo.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+@RequiredArgsConstructor
+public class HelloController {
+
+    @GetMapping
+    public String hello() {
+        return "hello";
+    }
+}
